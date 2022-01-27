06fb46165994cec800b9fc202b935fc5d4df8fa8
Cyrhades <cyrhades76@gmail.com>
2022-01-27 16:45:23 +0100

    Ajout du controle du JWT pour les APIs (aucun controle des roles / permissionspour le moment)

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,30 @@
 import express from 'express';
 import apiAuthRoutes from './auth/routes.js';
 import apiUsersRoutes from './users/routes.js';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 // Création de la route pour l'authentification
 router.use('/auth', apiAuthRoutes);
+
+// A partir d'ici on peut controler que le JWT est valide pour l'ensemble des routes
+router.use((req, res, next) => {
+    // Validation du JWT
+    if (req.headers.authorization) {
+        // On récupère le JWT dans le header
+        const token = req.headers.authorization.split(' ')[1] || null;
+        if(token) {
+            try{
+                if(jwt.verify(token, process.env.JWT_SECRET_KEY)) {
+                    next();
+                    return;
+                }
+            } catch(error) {}
+        }
+    }
+    res.status(403).send(); 
+});
 
 // Chargement des routes pour la collection users
 router.use('/users', apiUsersRoutes);
