47f3b535aa5c92c359fbdf5a214ca0788ff0b750
Cyrhades <cyrhades76@gmail.com>
2022-01-30 15:53:43 +0100

    amélioration de la gestion des tests

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 
 const app = express();
 
+// pour permettre le chargement des variables d'environnement
+dotenv.config({ path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env' });
 
 // permet de parser le contenu du body des requêtes, 
 // l'option extended true permet de parser les objets
@@ -11,7 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 //--------------------------------------------------------------------
 //      Chargement de MongoDB
 //--------------------------------------------------------------------
-
 mongoose.connect(
     process.env.MONGODB_URI, 
     {connectTimeoutMS : 3000, socketTimeoutMS: 20000, useNewUrlParser: true, useUnifiedTopology: true }
@@ -26,6 +27,8 @@ app.use('/api', apiRoutes);
 //--------------------------------------------------------------------
 //     Ecoute du serveur HTTP
 //--------------------------------------------------------------------
-app.listen(process.env.PORT,() => {
+app.listen(process.env.PORT, () => {
     console.log(`Le serveur est démarré : http://localhost:${process.env.PORT}`);
 });
+
+export default app;
