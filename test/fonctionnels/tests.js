7c5ba077cd27765c7c8159d36c7c024b1e950401
Cyrhades <cyrhades76@gmail.com>
2022-02-04 14:49:07 +0100

    Ajout de tests fonctionnels

--- test/fonctionnels/tests.js
+++ test/fonctionnels/tests.js
@@ -1,7 +1,10 @@
 import Auth from './auth.js';
+import Users from './users.js';
 
 export default () => {
     describe(`🌍 Tests Fonctionnels de l'API`, function() {
         describe('🔑 Authentification Api', Auth.bind(this));
+
+        describe('🧑‍💻 Utilisateurs', Users.bind(this));
     });
 };
